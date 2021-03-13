02f45bb7c1305247a54d8c4937c7713e0533ad52
Vinicius Da Silva Cruz <vinicius.da.silva.cruz.sanji@gmail.com>
2021-03-12 22:31:52 -0300

    Criando o config service

--- fornecedor/fornecedor-web/src/main/java/br/com/study/fornecedor/FornecedorApplication.java
+++ fornecedor/fornecedor-web/src/main/java/br/com/study/fornecedor/FornecedorApplication.java
@@ -2,8 +2,10 @@ package br.com.study.fornecedor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FornecedorApplication {
 
     public static void main(String[] args) {
--- loja/loja-web/src/main/java/br/com/study/loja/LojaApplication.java
+++ loja/loja-web/src/main/java/br/com/study/loja/LojaApplication.java
@@ -2,8 +2,10 @@ package br.com.study.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LojaApplication {
 
     public static void main(String[] args) {
